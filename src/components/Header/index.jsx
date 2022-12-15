8214803d164c8c9c70cc72a15146067c9910971a
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-12-15 11:45:23 +0100

    create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/about">A propos</Link>
+        </nav>
+    )
+}
+
+export default Header
