a5b7f1a957c92c280029b6f142068e0f9011ad4d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-12-27 13:35:57 +0100

    add ratings

--- src/pages/Accomodation/index.jsx
+++ src/pages/Accomodation/index.jsx
@@ -2,10 +2,29 @@ import { useParams } from 'react-router-dom'
 import Carousel from '../../components/Carousel'
 import accomodations from "../../data/logements.json"
 import styles from '../../style/Accomodation.module.css'
+import starFilled from '../../assets/star-filled.svg'
+import starBlank from '../../assets/star-blank.svg'
 
 function Accomodation() {
     const { id } = useParams();
     let item = accomodations.filter((accomodation) => accomodation.id === id)
+
+    function Positive() {
+        return <img src={starFilled} alt="" />
+    }
+
+    function Negative() {
+        return <img src={starBlank} alt="" />
+    }
+
+    const filledStars = Array.from({ length: item[0].rating }, (_, index) => {
+        return <Positive key={index} />;
+    });
+
+    const blankStars = Array.from({ length: 5 - item[0].rating }, (_, index) => {
+        return <Negative key={index} />;
+    });
+
 
     return (
         <div>
@@ -19,6 +38,18 @@ function Accomodation() {
                     <div className={`${styles.hostname}`}>{item[0].host.name}</div>
                     <img className={`${styles.hostpicture}`} src={item[0].host.picture} alt="" />
                 </div>
+            </div>
+            <div className={`${styles.properties2}`}>
+                <ul className={`${styles.tags}`}>
+                    {item[0].tags.map((tag) => (
+                        <li className={`${styles.tag}`} key={tag}>{tag}</li>
+                    ))}
+                </ul>
+                <div >
+                    {filledStars} {blankStars}
+                </div>
+
+
             </div>
         </div>
     )
