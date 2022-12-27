43d04b9bc6bca2cc90ffd324fedfb6f7bed56ee2
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-12-27 10:54:53 +0100

    add accomodations properties

--- src/pages/Accomodation/index.jsx
+++ src/pages/Accomodation/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import Carousel from '../../components/Carousel'
 import accomodations from "../../data/logements.json"
+import styles from '../../style/Accomodation.module.css'
 
 function Accomodation() {
     const { id } = useParams();
@@ -9,6 +10,16 @@ function Accomodation() {
     return (
         <div>
             <Carousel pictures={item[0].pictures}></Carousel>
+            <div className={`${styles.properties}`}>
+                <div className={`${styles.accomodation}`}>
+                    <h2 className={`${styles.title}`}>{item[0].title}</h2>
+                    <div className={`${styles.location}`}>{item[0].location}</div>
+                </div>
+                <div className={`${styles.host}`}>
+                    <div className={`${styles.hostname}`}>{item[0].host.name}</div>
+                    <img className={`${styles.hostpicture}`} src={item[0].host.picture} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
