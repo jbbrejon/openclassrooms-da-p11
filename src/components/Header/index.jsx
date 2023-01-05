dfbc0f6b3bb85daa9ef137327a3a67d57e17a23d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-05 12:54:54 +0100

    Add Header elements (logo + nav) and import CSS module

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,22 @@
+// Import Reactr module
+import { Link } from 'react-router-dom'
+
+// Import assets
+import logo from '../../assets/logo.svg'
+
+// Import CSS module
+import styles from '../../styles/Header.module.css'
+
+
+
 function Header() {
     return (
-        <header>
-            Header
+        <header className={`${styles.header}`}>
+            <img className={`${styles.logo}`} src={logo} alt="logo" />
+            <nav className={`${styles.nav}`}>
+                <Link className={`${styles.link}`} to="/">Accueil</Link>
+                <Link className={`${styles.link}`} to="/a-propos">A propos</Link>
+            </nav>
         </header>
     )
 }
