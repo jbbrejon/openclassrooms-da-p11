564a839d034b3f53e7a99656f66c38af4c354b2a
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-05 19:56:31 +0100

    Add Collapse component elements

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,67 @@
+// Import assets
+import chevronUp from '../../assets/chevron-up.svg'
+import chevronDown from '../../assets/chevron-down.svg'
+
+// Import React module
+import { useState } from 'react'
+
+// Import CSS moudule
+import styles from '../../styles/Collapse.module.css'
+
+
+function Collapse({ styleH, styleP, title, description }) {
+    const [isCollapsed, setIsCollapsed] = useState(true)
+    let cssH = styleH;
+    let cssP = styleP;
+    function handleClick() {
+        if (isCollapsed === true) {
+            setIsCollapsed(false);
+        }
+        else {
+            setIsCollapsed(true);
+        }
+    }
+
+    function isArray() {
+        return Array.isArray(description)
+    }
+
+    return isCollapsed ? (
+        <div className={`${styles.dropdown}`} >
+            <div className={`${styles.bar}`} >
+                <h2 className={`${styles[cssH]}`}>{title}</h2>
+                <img className={`${styles.chevron}`} src={chevronDown} alt="chevron-down" onClick={() => handleClick()} />
+            </div>
+        </div>
+    )
+        : (
+
+            isArray() ? (
+                <div className={`${styles.dropdown}`} >
+                    <div className={`${styles.bar}`} >
+                        <h2 className={`${styles[cssH]}`}>{title}</h2>
+                        <img className={`${styles.chevron}`} src={chevronUp} alt="chevron-up" onClick={() => handleClick()} />
+                    </div>
+
+                    <ul className={`${styles[cssP]}`}>
+                        {description.map((el) => (
+                            <li key={el}>{el}</li>
+                        ))}
+                    </ul>
+
+                </div>
+            )
+                : (
+                    <div className={`${styles.dropdown}`} >
+                        <div className={`${styles.bar}`} >
+                            <h2 className={`${styles[cssH]}`}>{title}</h2>
+                            <img className={`${styles.chevron}`} src={chevronUp} alt="chevron-up" onClick={() => handleClick()} />
+                        </div>
+                        <p className={`${styles[cssP]} ${styles.para}`}>{description}</p>
+                    </div>
+                )
+        )
+
+}
+
+export default Collapse
