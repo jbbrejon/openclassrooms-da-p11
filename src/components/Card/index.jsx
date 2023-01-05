485be079e2724c4c1088f243d6e843ad14b84cbb
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-05 13:25:34 +0100

    create Card component (cover + title w/ link to accomodation page)

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,20 @@
+// Import React modules
+import { Link } from 'react-router-dom'
+
+// Import CSS module
+import styles from '../../styles/Card.module.css'
+
+function Card({ id, title, cover }) {
+    return (
+        <li className={`${styles.card}`}>
+            <div className={`${styles.background}`}>
+                <img className={`${styles.cover}`} src={cover} alt="" />
+            </div>
+            <h2 className={`${styles.title}`}>
+                <Link className={`${styles.link}`} to={`/accomodation/${id}`}>{title}</Link>
+            </h2>
+        </li>
+    )
+}
+
+export default Card
