d03aa927dc6db53fccd38932900213d6e441b21c
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-05 12:55:47 +0100

    Add Footer elements (logo + info) and import CSS module

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
+// Import assets
+import logo from '../../assets/logo--white.svg'
+
+// Import CSS module
+import styles from '../../styles/Footer.module.css'
+
 function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className={`${styles.footer}`}>
+            <img className={`${styles.logo}`} src={logo} alt="logo" />
+            <div className={`${styles.info}`} >© 2020 Kasa. All rights reserved</div>
         </footer>
     )
 }
