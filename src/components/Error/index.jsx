da013535353841849911ffc4744b2cbc17dd9ddc
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-05 13:03:48 +0100

    Add Error elements (error + message + link to homepage) and import CSS module

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,15 @@
+// Import React module
+import { Link } from "react-router-dom"
+
+// Import CSS module
+import styles from '../../styles/Error.module.css'
+
 function Error() {
     return (
-        <main>
-            Error
+        <main className={`${styles.main}`}>
+            <div className={`${styles.error}`}>404</div>
+            <div className={`${styles.message}`}>Oups! La page que vous demandez n'existe pas</div>
+            <Link className={`${styles.link}`} to="/">Retourner sur la page d'accueil</Link>
         </main>
     )
 }
