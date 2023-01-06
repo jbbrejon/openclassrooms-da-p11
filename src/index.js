9e8f3782c6984ba22353633a7f1ba82e94e55473
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-06 10:21:37 +0100

    Remove empty lines

--- src/index.js
+++ src/index.js
@@ -7,15 +7,8 @@ import Router from './utils/Router';
 // Import css
 import './index.css';
 
-
-
 // Set root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-
-
-
 
 root.render(
   <React.StrictMode>
