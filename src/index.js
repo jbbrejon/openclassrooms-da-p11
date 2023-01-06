62a6f735c0b1ed2690363226229dd3a6a4d7f2f3
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-06 10:19:57 +0100

    move router settings to dedicated file (utils\Router)

--- src/index.js
+++ src/index.js
@@ -1,70 +1,24 @@
 // Import React modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { RouterProvider, } from "react-router-dom";
+import Router from './utils/Router';
 
 // Import css
 import './index.css';
 
-// Import components
-import Header from './components/Header';
-import Error from './components/Error';
-import Footer from './components/Footer';
 
-// Import pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Accomodation from './pages/Accomodation';
 
 // Set root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Set standard layout (Header/Page/Footer)
-const Layout = () => (
-  <>
-    <div className='container'>
-      <Header />
-      <Outlet />
-    </div>
-    <Footer />
-  </>
-);
 
-// Set error layout (Header/Error component/Footer)
-const ErrorLayout = () => (
-  <>
-    <div className='container'>
-      <Header />
-      <Error />
-      <Outlet />
-    </div>
-    <Footer />
-  </>
-);
 
-// Router configuration
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    errorElement: <ErrorLayout />,
-    children: [{
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "a-propos",
-      element: <About />,
-    },
-    {
-      path: "logement/:id/",
-      element: <Accomodation />,
-    },]
-  }
-]);
+
 
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
